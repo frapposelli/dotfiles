a3a57602d05a282b0e04d3447ae828387be888cd
Fabio Rapposelli <fabio@rapposelli.org>
2015-05-25 16:44:13 +0200

    new packages for atom

--- tag-fish/config/fish/config.fish
+++ tag-fish/config/fish/config.fish
@@ -1,3 +1,11 @@
+set -gx P4EDITOR vi
+
+set -gx P4CONFIG .p4config
+
+set -gx P4PORT perforce.eng.vmware.com 1666
+
+set -gx PATH $PATH /build/trees/bin /build/apps/bin /build/toolchain/mac32/perforce-r14.1
+
 set -gx GOPATH ~/Development/goworkspace
 
 set -gx PATH ~/.bin $GOPATH/bin $PATH
