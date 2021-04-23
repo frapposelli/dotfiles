c7552370ceff66b3c1fd297627dd6be5d41d5e27
Fabio Rapposelli <fabio@vmware.com>
2021-04-23 09:35:04 +0200

    updates for 2021

--- tag-mac/config/fish/config.fish
+++ tag-mac/config/fish/config.fish
@@ -9,18 +9,22 @@ switch (uname)
 		set -gx PATH ~/Development/google-cloud-sdk/bin /Library/TeX/texbin $PATH
 		set -g fish_user_paths "/usr/local/opt/go@1.12/bin"
 		set -gx SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+		set -gx LC_ALL en_US.UTF-8
+		set -gx LC_CTYPE en_US.UTF-8
 		alias git=hub
 		alias ccat="pygmentize -O style=monokai -f console256 -g"
 		alias g=git
 		alias d=docker
 		eval (direnv hook fish)
-		. (rbenv init -|psub)
+		status --is-interactive; and source (rbenv init -|psub)
+		status --is-interactive; and source (pyenv init -|psub)
 	case "Linux"
 end
 alias g=git
 alias d=docker
 set -gx PATH ~/.bin $GOPATH/bin $HOME/.fastlane/bin /usr/local/sbin ~/Library/Android/sdk/platform-tools $PATH
 set -gx ANDROID_SDK /Users/fabio/Library/Android/sdk
+set -gx LC_ALL en_US.UTF-8
 
 function nvm
    bass source (brew --prefix nvm)/nvm.sh --no-use ';' nvm $argv
