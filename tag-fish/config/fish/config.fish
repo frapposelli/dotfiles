34fb0c3687b719a73150309e0db26f68c45d2cf8
Fabio Rapposelli <fabio@rapposelli.org>
2023-07-05 22:30:45 +0200

    fix iterm prefs

--- tag-fish/config/fish/config.fish
+++ tag-fish/config/fish/config.fish
@@ -77,8 +77,3 @@ end
 
 fish_add_path /usr/local/opt/binutils/bin
 
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-eval /usr/local/anaconda3/bin/conda "shell.fish" "hook" $argv | source
-# <<< conda initialize <<<
-
