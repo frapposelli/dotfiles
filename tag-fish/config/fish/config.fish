3d1abd49ba8084cd9c31b9b22d60a774d9545a2a
Fabio Rapposelli <fabio@rapposelli.org>
2023-07-25 10:09:41 +0200

    add getmail + devonthink scripts

--- tag-fish/config/fish/config.fish
+++ tag-fish/config/fish/config.fish
@@ -5,7 +5,7 @@ set -gx LC_ALL en_US.UTF-8  # Adjust this to your locale!
 set -gx VIRTUAL_ENV_DISABLE_PROMPT true
 # set -gx SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
 set -gx GOPATH $HOME/Development/goworkspace
-set -x PATH ~/.bin $GOPATH/bin $PATH
+set -x PATH ~/.bin ~/.scripts $GOPATH/bin $PATH
 set -gx HOMEBREW_AUTO_UPDATE_SECS 86400
 set -gx HOMEBREW_CASK_OPTS --appdir=/Applications
 set -gx DOCKER_BUILDKIT 1
