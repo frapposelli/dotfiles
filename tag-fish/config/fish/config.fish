96394d895d73a45b7ff80e41afe148eadc3fdc76
Fabio Rapposelli <fabio@rapposelli.org>
2024-07-19 16:38:16 +0200

    cleanup

--- tag-fish/config/fish/config.fish
+++ tag-fish/config/fish/config.fish
@@ -73,7 +73,20 @@ if which goenv > /dev/null; status --is-interactive; and source (goenv init -|ps
 if which rbenv > /dev/null; status --is-interactive; and source (rbenv init -|psub); end
 if which swiftenv > /dev/null; status --is-interactive; and source (swiftenv init -|psub); end
 if which pyenv > /dev/null; status is-login; and pyenv init --path | source; end
-if which op > /dev/null; status --is-interactive; and source /Users/fabio/.config/op/plugins.sh; end
 
 fish_add_path /usr/local/opt/binutils/bin
+
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /opt/homebrew/Caskroom/miniconda/base/bin/conda
+    eval /opt/homebrew/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
+        . "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/opt/homebrew/Caskroom/miniconda/base/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
 
