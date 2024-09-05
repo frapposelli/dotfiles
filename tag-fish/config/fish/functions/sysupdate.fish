7da370f2efef66a525105d989cc9bf561d24a410
Fabio Rapposelli <fabio@rapposelli.org>
2024-09-05 16:39:23 +0200

    apply changes when there's new dotfiles

--- tag-fish/config/fish/functions/sysupdate.fish
+++ tag-fish/config/fish/functions/sysupdate.fish
@@ -1,15 +1,24 @@
 function sysupdate
   if command -s toilet > /dev/null; toilet -f pagga "sysupdate"; end
   if command -s git > /dev/null; and test -d $HOME/.dotfiles
+	if command -s toilet > /dev/null
+		toilet -f wideterm "* updating dotfiles"
+	end
 	cd $HOME/.dotfiles
 	git pull
+	if command -s rcup > /dev/null
+		rcup -t mac -t fish -t vim -t ssh -t tmux
+	end
 	cd -
   end
   if command -s brew > /dev/null
+	if command -s toilet > /dev/null
+		toilet -f wideterm "* updating homebrew"
+	end
 	brew bundle --global -q
 	brew update -q
 	brew upgrade -g -q
-	brew cleanup -q
+	brew cleanup -q --prune=all --scrub
   end
 end
 
