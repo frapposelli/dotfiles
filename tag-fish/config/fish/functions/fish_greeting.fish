970b5ab4a4984a2fa2bd38650e9d1d2d34a5b166
Fabio Rapposelli <fabio@rapposelli.org>
2024-09-05 15:40:31 +0200

    cosmetic changes to fish functions

--- tag-fish/config/fish/functions/fish_greeting.fish
+++ tag-fish/config/fish/functions/fish_greeting.fish
@@ -1,3 +1,4 @@
 function fish_greeting
-  if which fortune > /dev/null; and which cowsay > /dev/null; fortune | cowsay -T U -e oO; end
+  if command -s toilet > /dev/null; toilet -f pagga (hostname); end
+  # if which fortune > /dev/null; and which cowsay > /dev/null; fortune | cowsay -T U -e oO; end
 end
--- tag-fish/config/fish/functions/sysupdate.fish
+++ tag-fish/config/fish/functions/sysupdate.fish
@@ -1,12 +1,20 @@
 function sysupdate
-  if which toilet > /dev/null;toilet -f pagga "sysupdate"; end
-  if which brew > /dev/null; brew update; end
-  if which brew > /dev/null; brew upgrade -g; end
-  if which brew > /dev/null; brew cleanup; end
+  if command -s toilet > /dev/null; toilet -f pagga "sysupdate"; end
+  if command -s git > /dev/null; and test -d $HOME/.dotfiles
+	cd $HOME/.dotfiles
+	git pull
+	cd -
+  end
+  if command -s brew > /dev/null
+	brew bundle --global -q
+	brew update -q
+	brew upgrade -g -q
+	brew cleanup -q
+  end
 end
 
 function sysupdate_and_die
   sysupdate
-  if which toilet > /dev/null;toilet -f pagga "shutting down system"; end
+  if command -s toilet > /dev/null;toilet -f pagga "shutting down system"; end
   sudo shutdown -h now
 end
